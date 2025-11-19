ba8ec6d0668b1c7cd8e2c618d7049985dea181d9
MRNMT <nelsonmadileng57@gmail.com>
2025-11-19 12:01:58 +0200

    Revert "Development"

--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -1,56 +1,56 @@
 import express, { Request, Response } from "express";
-import * as userService from "../services/user.service.ts";
-import User from "../types/user.type";
+import * as userService from 
+import Accommodation from "../types/accommodation.type";
 
-export const getAllUsers = async (req: Request, res: Response) => {
+export const getAllAccommodations = async (req: Request, res: Response) => {
   try {
-    const users = await userService.getAll();
-    res.status(200).json(users);
+    const accommodations = await accommodationService.getAll();
+    res.status(200).json(accommodations);
   } catch (error) {
     console.error("Error in fetching all accomodations", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
-export const getUserById = async (req: Request, res: Response) => {
+export const getAccommodationById = async (req: Request, res: Response) => {
     try{
         const id = parseInt(req.params.id)
-        const user = await userService.getById(id);
-        res.status(200).json(user)
+        const accommodation = await accommodationService.getById(id);
+        res.status(200).json(accommodation)
     }catch(error){
-        console.log("Getting user error:", error)
+        console.log("Getting accommodation error:", error)
         res.status(404).json({message: "Accomodation not Found"})
     }
 }
 
-export const updateUser = async (req: Request, res: Response) => {
+export const updateAccommodation = async (req: Request, res: Response) => {
 
     const id = parseInt(req.params.id)
-    const data: Partial<User> = req.body
+    const data: Partial<Accommodation> = req.body
 
     try{
         
-        const updatedUser  = await userService.update(id, data);
+        const updatedAccommodation  = await accommodationService.update(id, data);
 
-        if(!updateUser){
+        if(!updateAccommodation){
             res.status(404).json({message: "Accomodation not found"})
         }
 
-        return res.status(200).json({updatedUser})
+        return res.status(200).json({updatedAccommodation})
     }catch(error){
         console.log("Error in updating", error)
     }
 }
 
-export const createUser = async (req: Request, res: Response) => {
+export const createAccommodation = async (req: Request, res: Response) => {
     
-    const data: Partial<User> = req.body
+    const data: Partial<Accommodation> = req.body
 
     try{
-        const createdUser  = userService.create(data)
+        const createdAccommodation  = accommodationService.create(data)
         res.status(201).json(data)
     }catch(error){
-        console.log("Error in adding an user", error)
+        console.log("Error in adding an accommodation", error)
         return res.status(400).json({message: "Bad Request"})
     }
 }
