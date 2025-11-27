6472eb986981b09f917cb2a3eba3d1cc9432ccd5
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2025-11-27 08:57:54 +0200

    Working on bookings, Room and user modals

--- CMS/src/components/AddBookingModal.tsx
+++ CMS/src/components/AddBookingModal.tsx
@@ -1,16 +1,14 @@
 import { Button } from "./Button";
-import logo from '../assets/logo.jpg'
 
-export const AddRoomModal = ({ onClose }: { onClose: () => void }) => {
+export const AddBookingModal = ({ onClose }: { onClose: () => void }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
 
       <div className="bg-white w-[500px] rounded-lg shadow-xl p-6 animate-fadeIn">
-        <img src={logo} className="w-10" alt="" />
 
-        <h1 className="text-2xl font-bold text-blue-500 mb-2">Add New Room</h1>
+        <h1 className="text-2xl font-bold text-gray-800 mb-2">Add New Boooking</h1>
         <p className="text-gray-500 mb-6">
-          Create a new room in your hotel . Fill in all the details below.
+          Create a new booking in your hotel . Fill in all the details below.
         </p>
 
         <form className="space-y-4">
@@ -70,3 +68,4 @@ export const AddRoomModal = ({ onClose }: { onClose: () => void }) => {
     </div>
   );
 };
+
